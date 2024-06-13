cd0f7e726d55c58ccd7293c47267b05183682d83
remauxl <yasin.saymaz@gmail.com>
2024-06-13 22:01:05 +0300

    error handler

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -10,7 +12,6 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
@@ -26,12 +27,11 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    stasu:'fail',
-    message:`Can't find ${req.originalUrl} on this server!`
-  });
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`));
 }
 );
+
+app.use(globalErrorHandler)
 
 
 module.exports = app;
